83b7cf9333b047ce288f2ba913d243642135d454
Christopher Byatt <christo.by@hotmail.com>
2025-08-01 12:41:07 -0400

    debut ex.3

---
+++ labo/Labo3/exercice_1_tableaux/fonctions.h
@@ -0,0 +1,12 @@
+//
+// Created by chris on 2025-08-01.
+//
+
+#ifndef FONCTIONS_H
+#define FONCTIONS_H
+
+#include <array>
+
+
+
+#endif //FONCTIONS_H
---
+++ labo/Labo3/exercice_1_tableaux/principal.cpp
@@ -0,0 +1,11 @@
+//
+// Created by chris on 2025-08-01.
+//
+
+#include "fonctions.h"
+using namespace std;
+
+int main(void) {
+
+srand(time(0))
+}
